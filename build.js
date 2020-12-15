66d1e2ce911bc78f78382b942a894e3c6f6ffc6c
xEricL <37921711+xEricL@users.noreply.github.com>
2020-12-14 20:16:43 -0500

    proper info in build script

--- build.js
+++ build.js
@@ -18,10 +18,10 @@ function getCurrentPlatform(){
 builder.build({
     targets: (process.argv[2] != null && Platform[process.argv[2]] != null ? Platform[process.argv[2]] : getCurrentPlatform()).createTarget(),
     config: {
-        appId: 'helioslauncher',
-        productName: 'Helios Launcher',
+        appId: 'CrystalMod Launcher',
+        productName: 'CrystalMod_Launcher',
         artifactName: '${productName}-setup-${version}.${ext}',
-        copyright: 'Copyright © 2018-2020 Daniel Scalzi',
+        copyright: 'Copyright © 2020 Crystal Development, LLC.',
         directories: {
             buildResources: 'build',
             output: 'dist'
